74e21299c56baf68f77f1d75ab144144ace295d1
Maple Li <lichunxia1987@hotmail.com>
2019-08-07 21:50:22 -0400

    prevent SE index

--- layouts/head.js
+++ layouts/head.js
@@ -10,6 +10,7 @@ export default ({ region, city }) => (
     </title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="noindex" />
     <link rel="icon" href="https://dt-cdn.net/images/favicon-32x32-32-26d70a5cff.png"></link>
     <link rel="stylesheet" href="https://www.dynatrace.com/common/css/main-5ee3187763.css" />
     <link href="https://www.dynatrace.com/common/css/print-bd69d927f4.css" rel="stylesheet" media="print" type="text/css" />
